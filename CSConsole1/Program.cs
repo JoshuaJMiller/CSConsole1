9ca8868eb891228cfb45096796f3083c21c543e6
joshua miller <joshua.miller1014@gmail.com>
2021-08-31 12:27:31 -0700

    init commit

---
+++ CSConsole1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!\n");
+            s = Console.ReadLine();
+            Console.WriteLine(s + '!');
+        }
+    }
+}
