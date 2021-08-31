94db6aa84b56d213d6dc6b36782ccf14a5aa1ca1
joshua miller <joshua.miller1014@gmail.com>
2021-08-31 14:45:42 -0700

    adds file writing and reading test

--- CSConsole1/Program.cs
+++ CSConsole1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CSConsole1
 {
@@ -6,11 +7,43 @@ namespace CSConsole1
     {
         static void Main(string[] args)
         {
-            String s;
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!!\n");
-            s = Console.ReadLine();
-            Console.WriteLine(s + '!');
+            Console.WriteLine("Enter filename >");
+
+            String filename = "..\\..\\..\\" + Console.ReadLine() + ".txt";
+
+            StreamWriter SW = new StreamWriter(filename);
+            Console.WriteLine("Enter first line to write to file.");
+            String line2 = Console.ReadLine();
+            SW.WriteLine(line2);
+            SW.Close();
+
+            Console.WriteLine("r - read file\nq - quit\n>");
+            String selection = Console.ReadLine();
+            if (selection == "r")
+            {
+
+                StreamReader SR = new StreamReader(filename);
+                String line = SR.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = SR.ReadLine();
+                }
+                SR.Close();
+                Console.WriteLine();
+
+                Console.Write(line);
+            }
+            else if (selection == "q") return;
+
+
+           
+
+            //String s;
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!!");
+            //s = Console.ReadLine();
+            //Console.WriteLine(s + '!');
         }
     }
 }
